35f44711c7d8083eb76bf8a97bf7a8e1f478ab16
gaurav-pndy <gauravpndy00@gmail.com>
2025-04-13 18:30:54 +0530

    updated gallery

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { useTranslation } from "react-i18next";
+import { Star } from "lucide-react";
 
 export default function Hero() {
   const { t } = useTranslation();
@@ -85,11 +86,31 @@ export default function Hero() {
 
           {/* Mini Pizza Card */}
           {/* <div className=" "> */}
-          <img
+          {/* <img
             src="/hero images/hero-pizza-card.png"
             alt="Cheese Pizza"
             className="h-26 md:h-34 lg:h-38 object-cover absolute bottom-0 right-0 md:right-32 xl:right-0 rounded-md"
-          />
+          /> */}
+          <div className="bg-[#181818] shadow-[6px_6px_10px_0px_rgba(0,0,0,0.25)]  text-white rounded-2xl absolute bottom-4 right-0 md:right-22 lg:right-40 md:bottom-10 xl:right-0 px-3 py-2 flex items-center gap-4 w-fit">
+            <img
+              src="https://static.wixstatic.com/media/df6cc5_1cf80df2c2a74b96a3b20157fbca571e~mv2.jpg" // Replace with the correct path to your image
+              alt="Royal Thali Veg"
+              className="w-14 md:w-22 h-14 md:h-22 object-cover rounded-2xl"
+            />
+            <div className="flex flex-col  gap-4 h-full">
+              <h3 className="text-sm md:text-lg  font-[500] font-poppins">
+                {t("hero.thali")}
+              </h3>
+              <div className="flex items-center  justify-between ">
+                {Array.from({ length: 5 }).map((_, i) => (
+                  <Star
+                    key={i}
+                    className="w-3 md:w-5 h-3 md:h-5 fill-yellow-400 stroke-yellow-400"
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
           {/* </div> */}
         </motion.div>
       </div>

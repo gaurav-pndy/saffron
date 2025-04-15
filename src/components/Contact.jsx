7739bbcdb055945eb3f7d30b94df3fb9b22f980d
gaurav-pndy <gauravpndy00@gmail.com>
2025-04-16 01:06:10 +0530

    ui updates

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -68,18 +68,18 @@ const ContactSection = () => {
                   rel="noopener noreferrer"
                   className="hover:scale-110 transition-all duration-300"
                 >
-                  <button className="p-2 rounded-full border-2 border-green-500  ">
-                    <FaWhatsapp size={24} className=" text-green-500" />
+                  <button className="p-2 rounded-full border-2 border-green-500  bg-green-500">
+                    <FaWhatsapp size={24} className=" text-black" />
                   </button>
                 </a>
 
                 <a
-                  href="https://t.me/saffronmoskva"
+                  href="https://eda.yandex.ru/restaurant/saffron_t8lmv"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="  rounded-md font-medium hover:scale-110 transition-all duration-300"
                 >
-                  <button className="p-1.5 rounded-full border-2 border-yellow-500 ">
+                  <button className="p-1.5 rounded-full border-2 border-yellow-500 bg-yellow-500">
                     <img src="/yandex.png" alt="" className="h-7 " />
                   </button>
                 </a>
